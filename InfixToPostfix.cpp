a07ea87e689bbabc2760b33407d5c5aaba4340c6
Anmol Mittal <mittalanmol22@gmail.com>
2019-02-01 23:16:50 +0530

    Infix to Postfix
    A simple program to convert infix string to postfix string.

---
+++ InfixToPostfix.cpp
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define SIZE 100
+
+char stack[SIZE];
+int top= -1;
+
+void push(char item)
+{
+    top= top+1;
+    stack[top]=item;
+}
+
+char pop()
+{
+    char item;
+    if(top==-1)
+    {
+        getchar();
+        exit(1);
+    }
+    
+    else
+    {
+        item= stack[top];
+        top= top-1;
+        return item;
+    }
+}
+
+int is_operator(char symbol)
+{
+    if(symbol == '^' || symbol == '*' || symbol == '/' || symbol == '+' || symbol =='-')
+    return 1;
+    
+    else 
+    return 0;
+}
+
+int priority(char symbol)
+{
+    if(symbol== '^')
+    return 3;
+    
+    else if(symbol== '*' || symbol== '/')
+    return 2;
+    
+    else if(symbol== '+' || symbol== '-')
+    return 1;
+    
+    else 
+    return 0;
+}
+
+void InfixToPostfix(char infix_exp[], char postfix_exp[])
+{
+    int i, j;
+    char item, x;
+    
+    push('(');
+    strcat(infix_exp, ")");
+    
+    i=0;
+    j=0;
+    item=infix_exp[i];
+    
+    while(item!='\0')
+    {
+        if(item=='(')
+        push(item);
+        
+        else if(isalnum(item))
+        {
+            postfix_exp[j]=item;
+            j++;
+        }
+        
+        else if(is_operator(item) == 1)
+        {
+            x=pop();
+            while(is_operator(x) == 1 && priority(x)>=priority(item))
+            {
+                postfix_exp[j]= x;
+                j++;
+                x=pop();
+            }
+            push(x);
+            push(item);
+        }
+        
+        else if(item== ')')
+        {
+            x=pop();
+            while(x != '(')
+            {
+                postfix_exp[j]= x;
+                j++;
+                x=pop();
+            }
+        }
+        else
+        {
+            getchar();
+            exit(1);
+        }
+        
+        i++;
+        item= infix_exp[i];
+    }
+    if(top>0)
+    {
+        getchar();
+        exit(1);
+    }
+    postfix_exp[j]='\0';
+}
+
+int main()
+{
+    char infix[SIZE], postfix[SIZE];
+    gets(infix);
+    
+    InfixToPostfix(infix,postfix);
+    
+    printf("\n");
+    puts(postfix);
+}
